91eac864dc551552ca856d3b17c74fab112e4443
pademio <ITITIU20140@student.hcmiu.edu.vn>
2021-10-04 16:15:40 +0700

    Update ex1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -16,6 +16,11 @@ int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
 	//Your codes here
-	
+	int n;
+	while(testcase != 0) {
+		n = testcase % 10;
+		printf("%d", n);
+		testcase /= 10;
+	}
 	return 0;
 }
